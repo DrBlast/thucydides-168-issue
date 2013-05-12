a4591769745932a400bfa23e659aed1352310d99
enichegovsky <enichegovsky@fininfor.ru>
2013-05-12 16:29:02 +0400

    thucydides issue 168

---
+++ src/test/java/test_gs/jbehave/LookupADefinitionSuite.java
@@ -0,0 +1,6 @@
+package test_gs.jbehave;
+
+import net.thucydides.jbehave.ThucydidesJUnitStories;
+import net.thucydides.jbehave.ThucydidesJUnitStory;
+
+public class LookupADefinitionSuite extends ThucydidesJUnitStory {}
